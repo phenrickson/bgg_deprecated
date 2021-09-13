ec042400e6f91d5b383be95eaa8f9da6786eac46
Phil Henrickson <Phil@Sydneys-iMac.local>
2021-09-13 14:35:23 -0500

    packages, settings, and theme used in project

---
+++ knitr_settings.R
@@ -0,0 +1,10 @@
+cat("applying default knitr settings \n")
+
+knitr::opts_chunk$set(echo = F)
+knitr::opts_chunk$set(message=F)
+knitr::opts_chunk$set(warning=F)
+knitr::opts_chunk$set(tidy=T)
+knitr::opts_chunk$set(fig.height=6)
+knitr::opts_chunk$set(fig.width=8)
+options(knitr.duplicate.label = "allow")
+
