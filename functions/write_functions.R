6b47d28b68862253127076540444cb080ac55c49
Phil Henrickson <Phil@Sydneys-iMac.local>
2021-09-20 14:21:53 -0500

    store all functions in one script

--- functions/write_functions.R
+++ functions/write_functions.R
@@ -27,4 +27,56 @@ get_bgg_data_from_github<-function(input_date) {
 }
 
 # dump
-dump("get_bgg_data_from_github", file="get_bgg_data_from_github.R")
+dump("get_bgg_data_from_github", file="functions/get_bgg_data_from_github.R")
+
+# function for pulling collection and making a data frame
+get_collection<-function(username_string) {
+        
+        # get collection data from specified users
+        collection_obj<-bggCollection$new(username = username_string)
+        
+        # expand
+        collection_obj$expand(variable_names = c("name",
+                                                 "type",
+                                                 "yearpublished",
+                                                 "rating",
+                                                 "numplays",
+                                                 "own",
+                                                 "preordered",
+                                                 "prevowned",
+                                                 "fortrade",
+                                                 "want",
+                                                 "wanttoplay",
+                                                 "wanttobuy",
+                                                 "wishlist",
+                                                 "wishlistpriority"))
+        
+        # convert to dataframe
+        collection_data<-collection_obj$data %>%
+                rename(game_id = objectid) %>%
+                mutate(username = username_string,
+                       date = Sys.Date(),
+                       name = gsub(",", " ", name, fixed = T)) %>%
+                mutate_if(is.logical, .funs = ~ case_when(. == T ~ 1,
+                                                          .== F ~ 0)) %>%
+                select(username,
+                       date,
+                       game_id,
+                       type,
+                       rating,
+                       own,
+                       preordered,
+                       prevowned,
+                       fortrade,
+                       want,
+                       wanttoplay,
+                       wanttobuy,
+                       wishlist,
+                       wishlistpriority)
+        
+        return(collection_data)
+        
+}
+
+# dump
+dump("get_collection", file="functions/get_collection.R")
