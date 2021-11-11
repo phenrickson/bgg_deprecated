27c5fdae53bb86e5edbd99e63ccfa84a989236ba
Phil Henrickson <Phil@Sydneys-iMac.local>
2021-11-11 17:40:01 -0600

    updated 11/11/21

--- functions/write_functions.R
+++ functions/write_functions.R
@@ -29,54 +29,67 @@ get_bgg_data_from_github<-function(input_date) {
 # dump
 dump("get_bgg_data_from_github", file="functions/get_bgg_data_from_github.R")
 
-# function for pulling collection and making a data frame
-get_collection<-function(username_string) {
-        
-        # get collection data from specified users
-        collection_obj<-bggCollection$new(username = username_string)
-        
-        # expand
-        collection_obj$expand(variable_names = c("name",
-                                                 "type",
-                                                 "yearpublished",
-                                                 "rating",
-                                                 "numplays",
-                                                 "own",
-                                                 "preordered",
-                                                 "prevowned",
-                                                 "fortrade",
-                                                 "want",
-                                                 "wanttoplay",
-                                                 "wanttobuy",
-                                                 "wishlist",
-                                                 "wishlistpriority"))
-        
-        # convert to dataframe
-        collection_data<-collection_obj$data %>%
-                rename(game_id = objectid) %>%
-                mutate(username = username_string,
-                       date = Sys.Date(),
-                       name = gsub(",", " ", name, fixed = T)) %>%
-                mutate_if(is.logical, .funs = ~ case_when(. == T ~ 1,
-                                                          .== F ~ 0)) %>%
-                select(username,
-                       date,
-                       game_id,
-                       type,
-                       rating,
-                       own,
-                       preordered,
-                       prevowned,
-                       fortrade,
-                       want,
-                       wanttoplay,
-                       wanttobuy,
-                       wishlist,
-                       wishlistpriority)
-        
-        return(collection_data)
-        
-}
+get_collection <-
+        function(username_string) {
+                
+                # get collection data from specified users
+                collection_obj<-bggCollection$new(username = username_string)
+                
+                # expand
+                collection_obj$expand(variable_names = c("name",
+                                                         "type",
+                                                         "yearpublished",
+                                                         "rating",
+                                                         "numplays",
+                                                         "own",
+                                                         "preordered",
+                                                         "prevowned",
+                                                         "fortrade",
+                                                         "want",
+                                                         "wanttoplay",
+                                                         "wanttobuy",
+                                                         "wishlist",
+                                                         "wishlistpriority"))
+                
+                # convert to dataframe
+                collection_data<-collection_obj$data %>%
+                        rename(game_id = objectid) %>%
+                        mutate(username = username_string,
+                               date = Sys.Date(),
+                               name = gsub(",", " ", name, fixed = T)) %>%
+                        mutate_if(is.logical, .funs = ~ case_when(. == T ~ 1,
+                                                                  .== F ~ 0)) %>%
+                        select(username,
+                               date,
+                               game_id,
+                               type,
+                               rating,
+                               own,
+                               preordered,
+                               prevowned,
+                               fortrade,
+                               want,
+                               wanttoplay,
+                               wanttobuy,
+                               wishlist,
+                               wishlistpriority)
+                
+                # check for duplicates
+                dupes = which(duplicated(collection_data$game_id)==T)
+                
+                if (length(dupes) > 0) {
+                        collection_data_out = collection_data[-dupes]
+                } else {
+                        collection_data_out = collection_data
+                }
+                
+                # convert to tibble
+                collection_data_out = collection_data_out %>%
+                        as_tibble()
+                
+                return(collection_data_out)
+                
+        }
 
 # dump
 dump("get_collection", file="functions/get_collection.R")
