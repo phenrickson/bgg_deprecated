580088e227d13ab2cda594c52c5eb9f8dd2d39c7
Phil Henrickson <Phil@Sydneys-iMac.local>
2021-12-12 21:28:57 -0600

    run report for individual games

--- run_game_reports.R
+++ run_game_reports.R
@@ -1,4 +1,3 @@
-
 # load necessary pieces
 source("load_packages.R")
 source("theme_phil.R")
@@ -20,24 +19,39 @@ run_game_report = function(input_ids)
         foreach(i=1:length(input_ids)) %do% {
                 rmarkdown::render(here::here("examine_comparables.Rmd"),
                                   params = list(id = names_df$game_id[i]),
-                                  output_file =  names_df$name_id[i],
+                                  output_file =  names_df$game_id[i],
                                   output_dir = "game_reports")
         }
 
 }
 
 # # select agme ids
-# id = 297978
-# 
+# id = 317511
+# id = 283155
+# id = 161533
+# id = 343905
+# id = 237179
+id = 229853
+
 # # run and produce report for selected ids
-# run_game_report(id)
+run_game_report(id)
 
 # get top 100 from today
 today = get_bgg_data_from_github(Sys.Date())
 
 ids = today %>%
-        head(100) %>%
+        arrange(desc(bayes_average)) %>%
         pull(game_id)
 
 # run and produce report for selected ids
-run_game_report(ids[1:50])
+run_game_report(ids[191:200])
+
+# # find game
+# today %>%
+#         filter(grepl("Terra Mystica", game_name))
+# 
+# # select agme ids
+# id = 124361
+# 
+# # run and produce report for selected ids
+# run_game_report(id)
